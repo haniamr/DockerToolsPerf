d98e845ea6ca30c0afe99b8097436fdb0281ede6
Cloud Computing Tools Lab <ddcctlab@microsoft.com>
2017-07-14 16:45:05 -0700

    add debugger and second warm run

--- build-core.ps1
+++ build-core.ps1
@@ -1,19 +1,19 @@
 function build
 {
-	Write-Host "docker-compose -f docker-compose.yml -p dockerperf kill" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose.yml -p dockerperf kill }
+	Write-Host "docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf kill" -ForegroundColor Yellow
+	$m = measure-command { docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf kill }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
-	Write-Host "docker-compose -f docker-compose.yml -p dockerperf down --rmi all --remove-orphans" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose.yml -p dockerperf down --rmi all --remove-orphans }
+	Write-Host "docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf down --rmi local --remove-orphans" -ForegroundColor Yellow
+	$m = measure-command { docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf down --rmi local --remove-orphans }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 	
-	Write-Host "dotnet build DockerPerf.sln /t:rebuild" -ForegroundColor Yellow
-	$m = measure-command { dotnet build DockerPerf.sln /t:rebuild }
+	Write-Host "msbuild .\DockerPerf.sln /t:rebuild" -ForegroundColor Yellow
+	$m = measure-command { msbuild .\DockerPerf.sln /t:rebuild }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
-	Write-Host "docker-compose -f docker-compose.yml -p dockerperf up -d --build" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose.yml -p dockerperf up -d --build }
+	Write-Host "docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf up -d --build" -ForegroundColor Yellow
+	$m = measure-command { docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf up -d --build }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
 	Write-Host "docker ps --filter ""status=running"" --filter ""name=dockerperf"" --format ""{{.ID}}"" -n 1" -ForegroundColor Yellow
@@ -21,6 +21,7 @@ function build
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
 	Write-Host "docker inspect --format='{{(index (index .NetworkSettings.Ports ""80/tcp"") 0).HostPort}}' $id" -ForegroundColor Yellow
+	$port = "";
 	$m = measure-command { $port=(docker inspect --format='{{(index (index .NetworkSettings.Ports \"80/tcp\") 0).HostPort}}' $id) }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
@@ -40,16 +41,22 @@ function build
 			{
 			}
 			
-			Start-Sleep 0.1
+			Start-Sleep 0.2
 		}
 	}
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green
 }
 
+#
+# Pre-requisites
+#
 Write-Host "dotnet restore..." -ForegroundColor Yellow
 dotnet restore DockerPerf.sln
 
-$m = measure-command { build }
+# remove old images
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -p dockerperf down --rmi all --remove-orphans
+
+$e2e = measure-command { build }
 
 Write-Host
-Write-Host E2E Time: $m.Seconds seconds -ForegroundColor Green
+Write-Host E2E Time: $e2e.Seconds seconds -ForegroundColor Green
--- build-fx.ps1
+++ build-fx.ps1
@@ -1,27 +1,52 @@
-function build
+function build($clean)
 {
-	Write-Host "docker-compose -f docker-compose-fx.yml -p dockerperffx kill" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose-fx.yml -p dockerperffx kill }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	if ($clean) {
+		# Kill container
+		Write-Host "docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx kill" -ForegroundColor Yellow
+		$m = measure-command { docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx kill }
+		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
-	Write-Host "docker-compose -f docker-compose-fx.yml -p dockerperffx down --rmi all --remove-orphans" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose-fx.yml -p dockerperffx down --rmi all --remove-orphans }
+		# Remove old images
+		Write-Host "docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx down --rmi local --remove-orphans" -ForegroundColor Yellow
+		$m = measure-command { docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx down --rmi local --remove-orphans }
+		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	}
+	
+	# docker-compose config, the result is not used in the script but in VS scenario, just keep this here to mimic the process
+	Write-Host "docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx config" -ForegroundColor Yellow
+	$m = measure-command { docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx config }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 	
+	# build the project
 	Write-Host "msbuild DockerPerfFx.sln /t:rebuild" -ForegroundColor Yellow
 	$m = measure-command { msbuild DockerPerfFx.sln /t:rebuild }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
-	Write-Host "docker-compose -f docker-compose-fx.yml -p dockerperffx up -d --build" -ForegroundColor Yellow
-	$m = measure-command { docker-compose -f docker-compose-fx.yml -p dockerperffx up -d --build }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
-
+	if ($clean) {
+		# build and start container
+		Write-Host "docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx up -d --build" -ForegroundColor Yellow
+		$m = measure-command { docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx up -d --build }
+		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	} else {
+		# make sure container is up-to-date by calling docker compose up
+		Write-Host "docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx up -d" -ForegroundColor Yellow
+		$m = measure-command { docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx up -d }
+		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	}
+	
+	# get container ID
 	Write-Host "docker ps --filter ""status=running"" --filter ""name=dockerperffx"" --format ""{{.ID}}"" -n 1" -ForegroundColor Yellow
 	$m = measure-command { $id=(docker ps --filter "status=running" --filter "name=dockerperffx" --format "{{.ID}}" -n 1) }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
-	Write-Host "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}""" -ForegroundColor Yellow
+	# get IP address
+	Write-Host "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}"" $id" -ForegroundColor Yellow
 	$m = measure-command { $ip=(docker inspect --format="{{.NetworkSettings.Networks.nat.IPAddress}}" $id) }
+	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	
+	# start debugger if it is not already started
+	Write-Host "docker exec $id powershell -Command if ((Get-Process msvsmon -ErrorAction SilentlyContinue).Count -eq 0) {  Start-Process C:\remote_debugger\msvsmon.exe -ArgumentList /noauth, /anyuser, /silent, /nostatus, /noclrwarn, /nosecuritywarn, /nofirewallwarn, /nowowwarn, /timeout:2147483646}" -ForegroundColor Yellow
+	$m = measure-command { docker exec $id powershell -Command { if ((Get-Process msvsmon -ErrorAction SilentlyContinue).Count -eq 0) {  Start-Process C:\remote_debugger\msvsmon.exe -ArgumentList /noauth, /anyuser, /silent, /nostatus, /noclrwarn, /nosecuritywarn, /nofirewallwarn, /nowowwarn, /timeout:2147483646} } }
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
 	Write-Host "Pinging http://$ip/" -ForegroundColor Yellow
@@ -40,16 +65,63 @@ function build
 			{
 			}
 			
-			Start-Sleep 0.1
+			# cannot decrease this ping, if ping too frequently the container will freeze
+			Start-Sleep 1
 		}
 	}
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green
 }
 
+function codeChange 
+{
+	$path = pwd
+	$codePath = "$path\DockerPerfFx\Controllers\HomeController.cs"
+	$contents = [System.IO.File]::ReadAllText($codePath)
+	[System.IO.File]::WriteAllText($codePath, $contents.Replace("description", "more description"));
+}
+
+#
+# Pre-requisites
+#
 Write-Host "nuget restore..." -ForegroundColor Yellow
 .\nuget.exe restore DockerPerfFx.sln
 
-$m = measure-command { build }
+#
+# First run
+#
+Write-Host "First Run..." -ForegroundColor Green
+
+# Clean up old images
+docker-compose -f docker-compose-fx.yml -f docker-compose-fx.override.yml -p dockerperffx down --rmi all --remove-orphans
+
+$e2e = measure-command { 
+	build $true
+}
 
 Write-Host
-Write-Host E2E Time: $m.Seconds seconds -ForegroundColor Green
+Write-Host E2E Time: $e2e.Seconds seconds -ForegroundColor Green
+Write-Host
+Write-Host
+
+#
+# Second run
+#
+
+Write-Host "Second run..." -ForegroundColor Green
+
+# Simulate a code change
+Write-Host "Simulate a code change..." -ForegroundColor Green
+codeChange
+
+# Sleep 30 seconds to avoid file locking issue
+Write-Host "Sleep 30 seconds to avoid file locking issue..." -ForegroundColor Green
+Start-Sleep 30
+
+$e2e = measure-command { 
+	build $false
+}
+
+Write-Host
+Write-Host E2E Time: $e2e.Seconds seconds -ForegroundColor Green
+Write-Host
+Write-Host
