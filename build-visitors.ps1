86a233e6361703f815b4c7905e9473b8d4f21623
ddcctlab <ddcctlab@DOCKERPERF>
2017-08-22 17:39:11 -0500

    Simplify script

--- build-visitors.ps1
+++ build-visitors.ps1
@@ -1,70 +1,54 @@
 $dockerComposeArgs = "-f docker-compose-visitors.yml -f docker-compose-visitors.override.yml -p visitors"
 
+function runAndMeasure($command) {
+	Write-Host $command -ForegroundColor Yellow
+	$m = measure-command { $res = Invoke-Expression $command }
+	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	
+	return $res
+}
+
 function build($clean)
 {
 	if ($clean) {
 		# Kill container
-		Write-Host "docker-compose $dockerComposeArgs kill" -ForegroundColor Yellow
-		$m = measure-command { Invoke-Expression "docker-compose $dockerComposeArgs kill" }
-		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+		runAndMeasure "docker-compose $dockerComposeArgs kill"
 
 		# Remove old images
-		Write-Host "docker-compose $dockerComposeArgs down --rmi local --remove-orphans" -ForegroundColor Yellow
-		$m = measure-command { Invoke-Expression "docker-compose $dockerComposeArgs down --rmi local --remove-orphans" }
-		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+		runAndMeasure "docker-compose $dockerComposeArgs down --rmi local --remove-orphans"
 	}
 	
 	# docker-compose config, the result is not used in the script but in VS scenario, just keep this here to mimic the process
-	Write-Host "docker-compose $dockerComposeArgs config" -ForegroundColor Yellow
-	$m = measure-command { Invoke-Expression "docker-compose $dockerComposeArgs config" }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	runAndMeasure "docker-compose $dockerComposeArgs config"
 	
 	# build the project
 	if ($clean) {
-		Write-Host "msbuild Visitors/MyCompany.Visitors.Server.sln /t:rebuild" -ForegroundColor Yellow
-		$m = measure-command { msbuild Visitors/MyCompany.Visitors.Server.sln /t:rebuild }
+		runAndMeasure "msbuild Visitors/MyCompany.Visitors.Server.sln /t:rebuild"
 	} else {
-		Write-Host "msbuild Visitors/MyCompany.Visitors.Server.sln" -ForegroundColor Yellow
-		$m = measure-command { msbuild Visitors/MyCompany.Visitors.Server.sln }
+		runAndMeasure "msbuild Visitors/MyCompany.Visitors.Server.sln"
 	}
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
 
 	if ($clean) {
 		# build and start container
-		Write-Host "docker-compose $dockerComposeArgs up -d --build" -ForegroundColor Yellow
-		$m = measure-command { Invoke-Expression "docker-compose $dockerComposeArgs up -d --build" }
-		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+		runAndMeasure "docker-compose $dockerComposeArgs build --force-rm --no-cache"
+		
+		runAndMeasure "docker-compose $dockerComposeArgs up -d"
 	} else {
 		# make sure container is up-to-date by calling docker compose up
-		Write-Host "docker-compose $dockerComposeArgs up -d" -ForegroundColor Yellow
-		$m = measure-command { Invoke-Expression "docker-compose $dockerComposeArgs up -d" }
-		Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+		runAndMeasure "docker-compose $dockerComposeArgs up -d"
 	}
 	
 	# get container ID for web project
-	Write-Host "docker ps --filter ""status=running"" --filter ""name=visitors_mycompany.visitors.web_"" --format ""{{.ID}}"" -n 1" -ForegroundColor Yellow
-	$m = measure-command { $webid=(docker ps --filter "status=running" --filter "name=visitors_mycompany.visitors.web_" --format "{{.ID}}" -n 1) }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	$webid = runAndMeasure "docker ps --filter ""status=running"" --filter ""name=visitors_mycompany.visitors.web_"" --format ""{{.ID}}"" -n 1"
 	
 	# get container ID for webapi project
-	Write-Host "docker ps --filter ""status=running"" --filter ""name=visitors_mycompany.visitors.crmsvc_"" --format ""{{.ID}}"" -n 1" -ForegroundColor Yellow
-	$m = measure-command { $webapiid=(docker ps --filter "status=running" --filter "name=visitors_mycompany.visitors.crmsvc_" --format "{{.ID}}" -n 1) }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	$webapiid = runAndMeasure "docker ps --filter ""status=running"" --filter ""name=visitors_mycompany.visitors.crmsvc_"" --format ""{{.ID}}"" -n 1"
 
 	# get IP address for web project
-	Write-Host "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}"" $webid" -ForegroundColor Yellow
-	$m = measure-command { $webip=(docker inspect --format="{{.NetworkSettings.Networks.nat.IPAddress}}" $webid) }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	$webip = runAndMeasure "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}"" $webid"
 	
 	# get IP address for webapi project
-	Write-Host "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}"" $webapiid" -ForegroundColor Yellow
-	$m = measure-command { $webapiip=(docker inspect --format="{{.NetworkSettings.Networks.nat.IPAddress}}" $webapiid) }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
-	
-	# start debugger if it is not already started
-	Write-Host "docker exec $id powershell -Command if ((Get-Process msvsmon -ErrorAction SilentlyContinue).Count -eq 0) {  Start-Process C:\remote_debugger\msvsmon.exe -ArgumentList /noauth, /anyuser, /silent, /nostatus, /noclrwarn, /nosecuritywarn, /nofirewallwarn, /nowowwarn, /timeout:2147483646}" -ForegroundColor Yellow
-	$m = measure-command { docker exec $webid powershell -Command { if ((Get-Process msvsmon -ErrorAction SilentlyContinue).Count -eq 0) {  Start-Process C:\remote_debugger\msvsmon.exe -ArgumentList /noauth, /anyuser, /silent, /nostatus, /noclrwarn, /nosecuritywarn, /nofirewallwarn, /nowowwarn, /timeout:2147483646} } }
-	Write-Host $m.TotalSeconds seconds -ForegroundColor Green 
+	$webapiip = runAndMeasure "docker inspect --format=""{{.NetworkSettings.Networks.nat.IPAddress}}"" $webapiid"
 
 	Write-Host "Pinging http://$webip/noauth" -ForegroundColor Yellow
 	$m = measure-command { 
@@ -81,9 +65,7 @@ function build($clean)
 			catch
 			{
 			}
-			
-			# cannot decrease this ping, if ping too frequently the container will freeze
-			Start-Sleep 1
+			Start-Sleep 200
 		}
 	}
 	Write-Host $m.TotalSeconds seconds -ForegroundColor Green
