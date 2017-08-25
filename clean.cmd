3c555ec5462b0d2275788c0103be760918dc5df2
ddcctlab <ddcctlab@DOCKERPERF>
2017-08-25 14:04:56 -0500

    Fix nano to run in ctrl F5

--- clean.cmd
+++ clean.cmd
@@ -4,6 +4,8 @@ docker rmi -f dockerperf:latest
 docker rmi -f dockerperffx:latest
 docker rmi -f mycompany.visitors.web:latest
 docker rmi -f mycompany.visitors.crmsvc:latest
+docker rmi -f mycompany.visitors.web:dev
+docker rmi -f mycompany.visitors.crmsvc:dev
 
 git clean dockerperf/ -fdx
 git clean dockerperffx/ -fdx
